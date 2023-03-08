b4d44680b5bc15c03883cc187d1b25d2377d97aa
mabuissa24 <mabuissa24@amherst.edu>
2023-03-08 13:56:29 -0500

    Fix tester

--- tester.py
+++ tester.py
@@ -10,11 +10,11 @@ def test_pattern_check():
     pattern = wordlesolver.generate_pattern("array", "nahal")
     print(pattern)
     assert "nahal" in wordlesolver.pattern_check(pattern, "array"), "Pattern check not working, case 1."
-    # assert "jails" not in wordlesolver.pattern_check(pattern, "array"), "Pattern check not working, case 2."
+    assert "jails" not in wordlesolver.pattern_check(pattern, "array"), "Pattern check not working, case 2."
     print(len(wordlesolver.pattern_check(pattern, "array")))
 
     pattern = wordlesolver.generate_pattern("bruja", "jails")
-    assert "bruja" in wordlesolver.pattern_check(pattern, "jails"), "Pattern check not working, case 3."
+    assert "jails" in wordlesolver.pattern_check(pattern, "bruja"), "Pattern check not working, case 3."
 
 
 if __name__ == "__main__":
